95dbcea420d8731972e3eb2a9841b57442723487
mashashmidt <mashashmidtdesign@gmail.com>
2017-10-15 00:59:07 +0300

    * Главная страница

---
+++ js/index.js
@@ -0,0 +1,12 @@
+/*document.querySelector('.main-nav__toggle').addEventListener(
+    'click',
+    e => {
+        const menu = document.querySelector('.site-menu');
+        if (menu.classList.contains('site-menu--hidden')) {
+            menu.classList.remove('site-menu--hidden');
+        } else {
+            menu.classList.add('site-menu--hidden');
+        }
+    }
+);
+*/
